37cc97b0b58985565fb0a24f41764af8ecd1261c
Heshan Sandeepa <heshan1598@gmail.com>
2016-02-06 16:27:20 +0530

    Url session, Core Data and JSON

---
+++ Blog Reader/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Blog Reader
+//
+//  Created by Heshan Sandeepa on 2/6/16.
+//  Copyright © 2016 Heshan Sandeepa. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var webView: UIWebView!
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let myWebView = self.webView {
+                myWebView.loadHTMLString(detail.valueForKey("content")!.description, baseURL: NSURL(string: "https://googleblog.blogspot.com/"))
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
